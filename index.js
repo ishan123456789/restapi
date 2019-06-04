5c7b518d6565af986820407e3c7f069a2880b89b
ishan123456789 <ishumahajan94@gmail.com>
2019-06-04 17:43:37 +0530

    Parsed and fetch url

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+/**
+ * Common dependencies import these are by default node packages and not installed through npm
+ */
 const http = require('http');
+const url = require('url');
 
 let server = http.createServer((req, res) => {
     console.log('hello foo', req, res);
+
+    let parsedURL = url.parse(req.url, true);
+
+    let path = parsedURL.pathname.replace(/^\/+|\/$/gm, '');
+    // Sends response to the user else the request struck till timeout
     res.end('Got it?');
+    console.log('parsedURL', path);
 });
 
 server.listen(3000);
