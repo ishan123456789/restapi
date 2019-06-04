0ddaac7b51d458d4a35b2cff3c264c4f2e9c5f81
ishan123456789 <ishumahajan94@gmail.com>
2019-06-04 23:02:13 +0530

    Added methods to get QUERY String, Path, Headers, Payload Use Curl Request as: `curl -H "Authorization: Random Tatti" -H "Auth: Randomness in tatti" -H "Content-Type: application/json;" -d '{"k1": "sdaf", "k2": "31213" }' -X POST localhost:3000?tattiAgain=tatti`

--- index.js
+++ index.js
@@ -3,16 +3,30 @@
  */
 const http = require('http');
 const url = require('url');
+const stringDecoder = require('string_decoder').stringDecoder; // Will help us get the payload if any
 
 let server = http.createServer((req, res) => {
     console.log('hello foo', req, res);
 
     let parsedURL = url.parse(req.url, true);
-
+    
     let path = parsedURL.pathname.replace(/^\/+|\/$/gm, '');
+
+    let method = req.method;
+    console.log('method', method);
+    var queryStringObject = parsedURL.query;
+    var headerObject = req.headers;
+    console.log('queryString need to parse with JSON', queryStringObject);
+    console.log('headerObject need to parse with JSON', headerObject);
     // Sends response to the user else the request struck till timeout
-    res.end('Got it?');
-    console.log('parsedURL', path);
+    // var decoder = new stringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', (chunk) => buffer+=chunk);
+    req.on('end', _ => {
+        console.log('finalDataThatWas sent', buffer);
+        res.end('Got it?');
+        console.log('parsedURL', path);
+    });
 });
 
 server.listen(3000);
